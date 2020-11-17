a99ead21d468cddd8d9047c024f8bffb532da141
Joel Mora <joelemora@yahoo.com>
2020-11-16 19:15:27 -0500

    Testing login

--- server/routes/views/index.ts
+++ server/routes/views/index.ts
@@ -11,35 +11,33 @@ router.get('/', (req, res) => {
   res.render('pages/index');
 });
 
-router.get('/login', (req, res) => {
-  res.render('pages/login');
-});
+router.use('/login', (req, res) => {
+  if (req.method === 'GET') return res.render('pages/login');
+  if (req.method === 'POST') {
+    let user = {} as userLogin;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    if (!user.email)
+      return res.render('pages/login', {
+        error: "Missing Email"
+      });
+    if (!user.password)
+      return res.render('pages/login', {
+        error: "Missing Password"
+      });
 
-router.post('/login', (req, res) => {
-  let user = {} as userLogin;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  if (!user.email)
-    return res.render('pages/login', {
-      error: "Missing Email"
-    });
-  if (!user.password)
-    return res.render('pages/login', {
-      error: "Missing Password"
-    });
+    console.log("Logging in with " + user.email + " and " + user.password);
 
-  console.log("Logging in with " + user.email + " and " + user.password);
+    if (user.password === "invalid") {
+      return res.render('pages/login', {
+        error: "Invalid password"
+      });
+    }
 
-  if (user.password === "invalid") {
-    res.render('pages/login', {
-      error: "Invalid password"
-    });
-    return;
+    res.cookie("user", user.email);
+    res.redirect("/");
   }
-
-  res.cookie("user", req.body.email);
-
-  res.redirect("/");
+  return res.sendStatus(404);
 });
 
 router.get("/logout", (req, res) => {
